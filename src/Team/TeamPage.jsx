3744444d53e7332a03a9577f407943c7d9e5c585
yixuanqi <yixuan99316@gmail.com>
2022-01-18 17:20:38 -0800

    add shalaila

--- src/Team/TeamPage.jsx
+++ src/Team/TeamPage.jsx
@@ -69,7 +69,7 @@ export default function TeamPage(props) {
                             </Link>
                         </div>
                         <div className="col-md-4">
-                            <Link to={{ pathname: `/faye`}}  style={{ textDecoration: `none` }} target="_blank">
+                            <Link to={{ pathname: `/shalaila`}}  style={{ textDecoration: `none` }} target="_blank">
                                 <Card imgsrc={"https://raw.githubusercontent.com/yixuanQicq/BrainProject/main/src/Team/photos/Shalaila.jpg"} name={"Shalaila Hass"} title={"Neuroscientist"}/>
                             </Link>
                         </div>
